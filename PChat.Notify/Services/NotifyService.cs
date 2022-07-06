241be920a3b7ee177b3c9806f7679027b45a6530
4eck-qed <61916260+4eck-qed@users.noreply.github.com>
2022-07-06 08:23:01 +0200

    added debug messages

--- PChat.Notify/Services/NotifyService.cs
+++ PChat.Notify/Services/NotifyService.cs
@@ -15,25 +15,34 @@ public class NotifyService : Pchat.Notify.NotifyBase
 
     public override Task<ClientStatusResponse> NewFriend(User request, ServerCallContext context)
     {
+        if (Global.Debug)
+            _logger.LogDebug($"[Notify] {nameof(NewFriend)} called");
+
         var contact = new ContactCard {Id = request.Id};
         SessionContent.Contacts.Add(contact);
 
-        var response = new ClientStatusResponse();
-        return Task.FromResult(response);
+        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Contacts)));
+        return Task.FromResult(new ClientStatusResponse());
     }
 
     public override Task<ClientStatusResponse> Unfriended(User request, ServerCallContext context)
     {
+        if (Global.Debug)
+            _logger.LogDebug($"[Notify] {nameof(Unfriended)} called");
+
         var unfriendedUser = SessionContent.Contacts.FirstOrDefault(u => u.Id == request.Id);
         if (unfriendedUser != null)
             SessionContent.Contacts.Remove(unfriendedUser);
 
-        var response = new ClientStatusResponse();
-        return Task.FromResult(response);
+        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Contacts)));
+        return Task.FromResult(new ClientStatusResponse());
     }
 
     public override Task<ClientStatusResponse> FriendRequestReceived(FriendRequest request, ServerCallContext context)
     {
+        if (Global.Debug)
+            _logger.LogDebug($"[Notify] {nameof(FriendRequestReceived)} called");
+
         var friendRequest = new FriendRequest
         {
             Id = request.Id,
@@ -42,14 +51,16 @@ public class NotifyService : Pchat.Notify.NotifyBase
             TargetId = request.TargetId,
         };
         SessionContent.FriendRequests.Add(friendRequest);
+        
         EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.FriendRequests)));
-
-        var response = new ClientStatusResponse();
-        return Task.FromResult(response);
+        return Task.FromResult(new ClientStatusResponse());
     }
 
     public override Task<ClientStatusResponse> FriendRequestAnswered(FriendRequest request, ServerCallContext context)
     {
+        if (Global.Debug)
+            _logger.LogDebug($"[Notify] {nameof(FriendRequestAnswered)} called");
+
         // SessionContent.FriendRequests.FirstOrDefault(f => f.Id == request.Id)!.Status = request.Status;
         if (request.Status == FriendRequestStatus.Accepted)
         {
@@ -60,15 +71,16 @@ public class NotifyService : Pchat.Notify.NotifyBase
                 Status = "placeholder"
             });
         }
-        
-        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Contacts)));
 
-        var response = new ClientStatusResponse();
-        return Task.FromResult(response);
+        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Contacts)));
+        return Task.FromResult(new ClientStatusResponse());
     }
 
     public override Task<ClientStatusResponse> MessageReceived(TextMessage request, ServerCallContext context)
     {
+        if (Global.Debug)
+            _logger.LogDebug($"[Notify] {nameof(MessageReceived)} called");
+
         var message = new TextMessage
         {
             Id = request.Id,
@@ -78,10 +90,8 @@ public class NotifyService : Pchat.Notify.NotifyBase
             Time = request.Time,
         };
         SessionContent.Messages[request.SenderId].Add(message);
-        
-        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Messages)));
 
-        var response = new ClientStatusResponse();
-        return Task.FromResult(response);
+        EventBus.Instance.PostEvent(new OnObjectChangedEvent(nameof(SessionContent.Messages)));
+        return Task.FromResult(new ClientStatusResponse());
     }
 }
